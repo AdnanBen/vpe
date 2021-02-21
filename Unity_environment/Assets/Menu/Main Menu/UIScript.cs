61281979286b84df8eb11626c7cfa2017a91745d
mk7a <git@mk7.dev>
2021-02-21 10:22:26 +0000

    Files

---
+++ Unity_environment/Assets/Menu/Main Menu/UIScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.Main_Menu
+{
+    public class UIScript : MonoBehaviour
+    {
+
+        public void NewScene()
+        {
+            SceneManager.LoadScene("Selection Scene");
+        }
+    }
+}
---
+++ Unity_environment/Assets/University Classroom/Scripts/SceneInit.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace University_Classroom.Scripts
+{
+    public class SceneInit : MonoBehaviour
+    {
+    
+        
+        public GameObject boardObject;
+        
+        public bool overrideConfig;
+        public bool enableBoard;
+        public int monitorId;
+    
+
+        void Start()
+        {
+            if (overrideConfig)
+            {
+                if (enableBoard)
+                {
+                    boardObject.SetActive(true);
+                    var texture = boardObject.GetComponent<uDesktopDuplication.Texture>();
+                    texture.monitorId = monitorId;
+
+                }
+            }
+        
+        }
+        
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
