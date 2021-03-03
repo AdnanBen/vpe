b76d1b4665bb2ef1cb0d0879f76ac691ed5b5b3d
mk7a <git@mk7.dev>
2021-03-03 15:36:51 +0000

    update

---
+++ Unity_environment/Assets/Menu/Main Menu/ButtonScripts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Menu.Main_Menu
+{
+    public class ButtonScripts : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Unity_environment/Assets/Menu/Main Menu/ToggleImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ToggleImage : MonoBehaviour
+{
+    public Toggle tog;
+    public GameObject uiComponent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tog = GetComponent<Toggle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void onToggleChange() {
+        uiComponent.SetActive(!uiComponent.activeSelf);
+    }
+}
