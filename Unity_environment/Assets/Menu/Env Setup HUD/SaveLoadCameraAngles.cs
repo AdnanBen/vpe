7f8972200640e23d9e963d376df5e3e3ce7e7de0
AdnanBen <adnanb1112@gmail.com>
2021-03-16 08:01:32 +0000

    added more camera angle slots

--- Unity_environment/Assets/Menu/Env Setup HUD/SaveLoadCameraAngles.cs
+++ Unity_environment/Assets/Menu/Env Setup HUD/SaveLoadCameraAngles.cs
@@ -15,7 +15,7 @@ namespace University_Classroom.Scripts
             public float rotationy;
         }
         
-        cameraPosition[] cameraPositions = new cameraPosition[9];
+        cameraPosition[] cameraPositions = new cameraPosition[11];
 
         void Update()
         {
@@ -75,6 +75,139 @@ namespace University_Classroom.Scripts
             transform.position = cameraPositions[3].position;
             rotation.x = cameraPositions[3].rotationx;
             rotation.y = cameraPositions[3].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[4].position = transform.position;
+                    cameraPositions[4].rotationx = rotation.x;
+                    cameraPositions[4].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha4)) 
+        {
+            transform.position = cameraPositions[4].position;
+            rotation.x = cameraPositions[4].rotationx;
+            rotation.y = cameraPositions[4].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha5))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[5].position = transform.position;
+                    cameraPositions[5].rotationx = rotation.x;
+                    cameraPositions[5].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha5)) 
+        {
+            transform.position = cameraPositions[5].position;
+            rotation.x = cameraPositions[5].rotationx;
+            rotation.y = cameraPositions[5].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha6))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[6].position = transform.position;
+                    cameraPositions[6].rotationx = rotation.x;
+                    cameraPositions[6].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha6)) 
+        {
+            transform.position = cameraPositions[6].position;
+            rotation.x = cameraPositions[6].rotationx;
+            rotation.y = cameraPositions[6].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha7))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[7].position = transform.position;
+                    cameraPositions[7].rotationx = rotation.x;
+                    cameraPositions[7].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha7)) 
+        {
+            transform.position = cameraPositions[7].position;
+            rotation.x = cameraPositions[7].rotationx;
+            rotation.y = cameraPositions[7].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha8))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[8].position = transform.position;
+                    cameraPositions[8].rotationx = rotation.x;
+                    cameraPositions[8].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha8)) 
+        {
+            transform.position = cameraPositions[8].position;
+            rotation.x = cameraPositions[8].rotationx;
+            rotation.y = cameraPositions[8].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha9))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[9].position = transform.position;
+                    cameraPositions[9].rotationx = rotation.x;
+                    cameraPositions[9].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha9)) 
+        {
+            transform.position = cameraPositions[9].position;
+            rotation.x = cameraPositions[9].rotationx;
+            rotation.y = cameraPositions[9].rotationy;
+            camera.transform.eulerAngles = rotation;
+            time = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Alpha0))
+            {
+                time += Time.deltaTime;
+                if (time > timeDown)
+                {
+                    cameraPositions[10].position = transform.position;
+                    cameraPositions[10].rotationx = rotation.x;
+                    cameraPositions[10].rotationy = rotation.y;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.Alpha0)) 
+        {
+            transform.position = cameraPositions[10].position;
+            rotation.x = cameraPositions[10].rotationx; 
+            rotation.y = cameraPositions[10].rotationy;
             camera.transform.eulerAngles = rotation;
             time = 0f;
         }
